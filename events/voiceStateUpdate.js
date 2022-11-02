e210d04155267142648070d96096e02736454cbb
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-02 12:47:58 +0530

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,8 +1,8 @@
-module.exports = async(client, oS, nS) => {
-if (!oS.channelId) return;
-let player = client.manager.players.get(nS.guild.id);
-if (oS && nS && oS.channelId !== nS.channelId && player && oS.id === client.user.id) {
-if (oS.channelId === player.voiceId) {
+module.exports = async(client, oldState, newState) => {
+if (!oldState.channelId) return;
+let player = client.manager.players.get(newState.guild.id);
+if (oldState && newState && oldState.channelId !== newState.channelId && player && oldState.id === client.user.id) {
+if (oldState.channelId === player.voiceId) {
 player.destroy();
     }
   }
