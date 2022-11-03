c9837fd7b489503ee05e376b6e743b206bf7ddcd
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-03 22:03:06 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const config = require("../config.js");
 module.exports = async(client) => {
 try{
 console.log(`${client.user.tag} ready`);
-setInterval(() => {client.user.setActivity(`${config.prefix}help`, {type: ActivityType.Listening})}, 60000);
+setInterval(() => {client.user.setActivity(`${config.prefix}play`, {type: ActivityType.Listening})}, 60000);
 } catch (error) {
 console.error(error);}
 };
