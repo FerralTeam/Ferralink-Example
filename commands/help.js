f2fc8a710a3ba68909296e33006ffefeb95e9708
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-06 23:09:28 +0530

    Made by RodX

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+   name: "help",
+   aliases: ["h"],
+   run: async (client, message, args) => {
+
+const embed = new EmbedBuilder()
+.setColor("Random")
+.setAuthor({ name: 'Commands', iconURL: client.user.displayAvatarURL(),  url: 'https://discord.gg/8n3yNqtPAE' })
+.addFields({
+name: 'Music', value: '\`play\`, \`stop\`, \`skip\`, \`volume\`, \`resume\`, \`pause\`'
+});
+return message.channel.send({embeds: [embed]});
+    }
+} 
