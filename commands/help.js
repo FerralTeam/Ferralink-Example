2ca2ac4b2cd9e53926449ce51f2a8c8556b77081
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-06 23:11:56 +0530

    Made by RodX

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,12 @@ module.exports = {
    name: "help",
    aliases: ["h"],
    run: async (client, message, args) => {
-
 const embed = new EmbedBuilder()
-.setColor("Random")
-.setAuthor({ name: 'Commands', iconURL: client.user.displayAvatarURL(),  url: 'https://discord.gg/8n3yNqtPAE' })
+.setAuthor({name: 'Commands', iconURL: client.user.displayAvatarURL()})
+.setColor('Random')
 .addFields({
-name: 'Music', value: '\`play\`, \`stop\`, \`skip\`, \`volume\`, \`resume\`, \`pause\`'
+name: 'Music',
+value: '\`play\`, \`stop\`, \`skip\`, \`volume\`, \`resume\`, \`pause\`'
 });
 return message.channel.send({embeds: [embed]});
     }
