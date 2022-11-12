2aefceaef6d4ed6fe491107aea2f6b785a0fd8e9
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-12 09:22:13 +0530

    Create disconnect.js

---
+++ commands/disconnect.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "disconnect",
+    aliases: ["dc"],
+    run: async (client, message, args) => {
+if (!message.member.voice.channel) return message.reply({content: "You Must Join A Voice Channel Before Using That Command!"});
+ 
+if (message.guild.members.me.voice.channel && message.member.voice.channel.id !== message.guild.members.me.voice.channel.id)
+return message.reply({content: "You must be in same voice channel to use this command!"});
+
+const player = client.manager.players.get(message.guild.id);
+if (!player) return message.reply({content: "Player is already disconnected."});
+
+player.destroy();
+return message.reply({content: "📤 Left the voice channel"});
+    }
+} 
