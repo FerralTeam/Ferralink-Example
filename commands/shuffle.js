54f19c4af0739c0d15c5e3a464e192929a4e0370
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-12 09:12:30 +0530

    Create shuffle.js

---
+++ commands/shuffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "shuffle",
+    aliases: [],
+    run: async (client, message, args) => {
+if (!message.member.voice.channel) return message.reply({content: "You Must Join A Voice Channel Before Using That Command!"});
+ 
+if (message.guild.members.me.voice.channel && message.member.voice.channel.id !== message.guild.members.me.voice.channel.id)
+return message.reply({content: "You must be in same voice channel to use this command!"});
+
+const player = client.manager.players.get(message.guild.id);
+if (!player) return message.reply({content: "Nothing playing right now."});
+
+player.queue.shuffle();
+return message.reply({content: "🔁 Queue has been shuffle."})
+    }
+} 
