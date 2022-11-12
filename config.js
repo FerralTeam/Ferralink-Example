a1128f08b26dbf09b6153089eed3c264573ec37d
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-12 08:58:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,13 @@ module.exports = {
       auth: "youshallpass",
       secure: false
     }],
+    Options: {
+     moveOnDisconnect: false, 
+     resumable: false, 
+     resumableTimeout: 60, 
+     reconnectTries: 10, 
+     restTimeout: 60000
+    },
     spotify: {
         clientID: "Your spotify clientID",
         clientSecret: "Your spotify clientSecret"
